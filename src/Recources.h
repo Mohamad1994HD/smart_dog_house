8a7e2392b52339eb4acda21bcf43036659a7bdb9
Mohamad1994HD <mohd1haddad@gmail.com>
2016-09-26 03:37:27 +0300

    Organized lib

--- src/Recources.h
+++ src/Recources.h
@@ -4,14 +4,14 @@
 #define ACTION_TIME 1000
 
 #include "Arduino.h"
-#include "IRSystem.h"
-#include "SerialExtractor.h"
-#include "Config.h"
-#include "PoweredDevice.h"
-#include "28BYJ48.h"
-#include "Dispenser.h"
-#include "Sensor.h"
-#include "DHT.h"
+#include <IRSystem.h>
+#include <SerialExtractor.h>
+#include <Config.h>
+#include <PoweredDevice.h>
+#include <28BYJ48.h>
+#include <Dispenser.h>
+#include <Sensor.h>
+#include <DHT.h>
 #include <Timer.h>
 
 #endif
